6d7167ebf2d9bfc64fefb6b3623bf25f36d9ff82
Jack Schumacher <jackrschumacher@gmail.com>
2025-12-01 20:09:20 -0600

    Updated .sh

--- Jack_Schumacher_Final.sh
+++ Jack_Schumacher_Final.sh
@@ -3,9 +3,13 @@
 # Jack Schumacher Bash Final project
 echo "Domain reconnisance tool"
 if ping -c 1 -W 2 1.1.1.1 > /dev/null 2>&1; then
-    echo "Checking network speed"
-    speedtest-cli --secure
+    echo "Checking network speed to ensure stability when running tool. This test will take 30 seconds."
+    iperf3 -c ping.online.net  -p 5200 -t 30
     read -p "Enter a domain to perform reconissance on: " domain
+        echo "Checking domain legitimacy using openssl"
+        echo "Domain legitimacy using openssl:" >> domain_results.txt
+        whois "$domain" | tee -a domain_results.txt
+        echo | openssl s_client -connect "$domain":443 2>/dev/null | openssl x509 -noout -dates -issuer| tee -a domain_results.txt
 
         echo "Listing domains owned by the same organization:"
         echo "Listing domains owned by the same organization:" >> domain_results.txt
