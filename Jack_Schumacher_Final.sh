b6451995db3ada120fc59af80914ba755d37897e
Jack Schumacher <jackrschumacher@gmail.com>
2025-12-01 18:41:11 -0600

    Updated to write to file

--- Jack_Schumacher_Final.sh
+++ Jack_Schumacher_Final.sh
@@ -8,10 +8,12 @@ if ping -c 1 -W 2 1.1.1.1 > /dev/null 2>&1; then
     read -p "Enter a domain to perform reconissance on: " domain
 
         echo "Listing domains owned by the same organization:"
-        amass intel -d "$domain" -whois -v
+        echo "Listing domains owned by the same organization:" >> domain_results.txt
+        amass intel -d "$domain" -whois -v | tee -a domain_results.txt
 
         echo "Listing subdomains:"
-        amass enum -d "$domain" -active -src -v
+        echo "Listing subdomains:" >> domain_results.txt
+        amass enum -d "$domain" -active -src -v | tee -a domain_results.txt
 
     else
         echo "Network not connected"
